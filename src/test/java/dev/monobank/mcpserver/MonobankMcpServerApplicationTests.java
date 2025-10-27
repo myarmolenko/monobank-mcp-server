19e5fdfd81d00487a1babe97c4e9d5474766eabb
Maksym Yarmolenko <maksymyarmolenk@gmail.com>
2025-10-27 02:12:11 +0200

    fix: fix context load test

--- src/test/java/dev/monobank/mcpserver/MonobankMcpServerApplicationTests.java
+++ src/test/java/dev/monobank/mcpserver/MonobankMcpServerApplicationTests.java
@@ -2,8 +2,12 @@ package dev.monobank.mcpserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"MONOBANK_API_TOKEN=dummy-test-token"
+})
 class MonobankMcpServerApplicationTests {
 
 	@Test
